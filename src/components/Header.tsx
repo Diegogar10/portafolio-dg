2d2a361bad6739e6b254b912a19803c2c538b9ed
Diego Garcia <diegospar2@gmail.com>
2023-02-21 16:56:34 -0500

    header component version 1.1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,12 @@ const Header = () => {
   return (
     <div className='header__container'>
       <img className='header__image' src={imgPefil} alt='Foto Diego Garcia'/>
-      <section>
-        <p></p>
-        <h2></h2>
-        <p></p>
-        <button></button>
+      <section className='header__info'>
+        <p>¡Hola!</p>
+        <h2>Soy Diego</h2>
+        <p><span>Frontend Devoloper & Control Enginier </span></p>
+        {/* <p>Bienvenidos a mi pagina personal. <br /> Quedate conmigo... te contare un poco de mi experiencia!</p> */}
+        <button>Acerca de mi</button>
       </section>
     </div>
   )
